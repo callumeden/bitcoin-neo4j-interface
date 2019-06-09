9f176cd376e4ec91d934406563748d64f792d684
Callum Eden <callum.eden15@imperial.ac.uk>
2019-06-09 13:13:53 +0100

    hardcoding files due to payload stuff

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -12,4 +12,4 @@ transaction_files_all=${transaction_files_all:1:${#transaction_files_all}-1}
 echo "Relation input files are ${transaction_files_all}"
 
 
-echo `curl http://localhost:8090/admin/clusterByInput?data=$transaction_files_all`
+echo `curl http://localhost:8090/admin/clusterByInput`
