8d81905f46c4fceef595487820b617f7c3b7cef2
Callum Eden <callum.eden15@imperial.ac.uk>
2019-06-19 11:37:52 +0100

    simplifying script

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-transaction_files_regex="../health-monitoring/import/data/bitcoin-csv-block-*/sample-transaction-data-*.csv"
-transaction_files_all=""
-
-for file in $transaction_files_regex; do
-    transaction_files_all=("${transaction_files_all},${file}")
-done
-
-transaction_files_all=${transaction_files_all:1:${#transaction_files_all}-1}
-
-echo "Relation input files are ${transaction_files_all}"
-
-
 echo `curl http://localhost:8090/admin/clusterByInput`
