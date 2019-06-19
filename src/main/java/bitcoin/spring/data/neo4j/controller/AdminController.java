bf67fce27d084cfedf016ad6d9d0f1890d90bf59
Callum Eden <callum.eden15@imperial.ac.uk>
2019-06-19 12:21:49 +0100

    bad commas

--- src/main/java/bitcoin/spring/data/neo4j/controller/AdminController.java
+++ src/main/java/bitcoin/spring/data/neo4j/controller/AdminController.java
@@ -19,12 +19,12 @@ public class AdminController {
     @GetMapping(path = "/clusterByInput")
     public HttpEntity clusterByInput() {
         String[] individualFiles = new String[]{"../health-monitoring/import/data/bitcoin-csv-block-0-200000/sample-transaction-data-unique.csv",
-                        ",../health-monitoring/import/data/bitcoin-csv-block-300001-350000/sample-transaction-data-unique.csv",
-                        ",../health-monitoring/import/data/bitcoin-csv-block-450001-570000/sample-transaction-data-unique.csv",
-                        ",../health-monitoring/import/data/bitcoin-csv-block-200001-250000/sample-transaction-data-unique.csv",
-                        ",../health-monitoring/import/data/bitcoin-csv-block-350001-375000/sample-transaction-data-unique.csv",
-                        ",../health-monitoring/import/data/bitcoin-csv-block-250001-300000/sample-transaction-data-unique.csv",
-                        ",../health-monitoring/import/data/bitcoin-csv-block-375001-450000/sample-transaction-data-unique.csv"};
+                        "../health-monitoring/import/data/bitcoin-csv-block-300001-350000/sample-transaction-data-unique.csv",
+                        "../health-monitoring/import/data/bitcoin-csv-block-450001-570000/sample-transaction-data-unique.csv",
+                        "../health-monitoring/import/data/bitcoin-csv-block-200001-250000/sample-transaction-data-unique.csv",
+                        "../health-monitoring/import/data/bitcoin-csv-block-350001-375000/sample-transaction-data-unique.csv",
+                        "../health-monitoring/import/data/bitcoin-csv-block-250001-300000/sample-transaction-data-unique.csv",
+                        "../health-monitoring/import/data/bitcoin-csv-block-375001-450000/sample-transaction-data-unique.csv"};
 
         return clusteringService.clusterByInput(individualFiles);
     }
